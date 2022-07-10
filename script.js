3be1160c32a722eb1ba2b6298dc7567f8339bc6b
Anigbo <anigbofredrick@gmail.com>
2022-07-10 10:32:18 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const list = document.querySelector("ul").value
+const input = document.querySelector("input").value
+const button = document.querySelector("button").value
+
+function  myFunction() {
+const addInput = document.querySelector("input").value
+ const input = document.querySelector("input").value = ""
+const listItems = document.createElement("li")
+const span = document.createElement("span")
+const deleteButton = document.createElement("button")
+
+listItems.appendChild(span)
+listItems.appendChild(deleteButton)
+
+span.textContent = addInput
+deleteButton.textContent = "Delete"
+const list = document.querySelector("ul")
+list.appendChild(listItems)
+
+deleteButton.addEventListener("click", function() {
+    listItems.removeChild(span)
+    list.removeChild(listItems)
+
+})
+input = document.querySelector("input").focus()
+}
+
