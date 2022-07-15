591ca9a7fd235da0d8df5eeda5ade25bfd909927
Fredrick Anigbo <anigbofredrick@gmail.com>
2022-07-15 20:41:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const list = document.querySelector("ul").value
 const input = document.querySelector("input").value
 const button = document.querySelector("button").value
+let link = document.querySelector("a").value
 
 function  myFunction() {
 const addInput = document.querySelector("input").value
- const input = document.querySelector("input").value = ""
+ let input = document.querySelector("input").value = ""
 const listItems = document.createElement("li")
 const span = document.createElement("span")
 const deleteButton = document.createElement("button")
@@ -12,20 +13,34 @@ const deleteButton = document.createElement("button")
 listItems.appendChild(span)
 listItems.appendChild(deleteButton)
 
+
 span.textContent = addInput
 deleteButton.textContent = "Delete"
 const list = document.querySelector("ul")
 list.appendChild(listItems)
 
+
+
+
+
+
+
 deleteButton.addEventListener("click", function() {
     listItems.removeChild(span)
     list.removeChild(listItems)
-
-})
-input = document.querySelector("input").focus()
+    
+    
+  })
  
- if(addInput == "") {
+
+  input = document.querySelector("input").focus()
+  
+  if(addInput === "") {
    listItems.remove()
   }
+  
+
+
+
 }
 
